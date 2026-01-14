c0decb972f84dfb441d4a816bb5494c243c93ed3
zgeniusecoders <zgeniusecoders@gmail.com>
2026-01-14 20:48:31 +0100

    feat(viewset): creation du ProductVS

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
 
-from api.viewset import CategoryVS
+from api.viewset import CategoryVS, ProductVS
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryVS, basename="categories")
+router.register(r'products', ProductVS, basename="products")
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/viewset.py
+++ api/viewset.py
@@ -2,18 +2,18 @@ from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.viewsets import ModelViewSet
 
-from api.models import Category
-from api.serializers import CategorySz, UpsertCategorySerializer
+from api.models import Category, Product
+from api.serializers import UpsertCategorySerializer, ProductSz, CategoryListSz
 
 
 @extend_schema_view(
     create=extend_schema(
         request=UpsertCategorySerializer,
-        responses=CategorySz
+        responses=CategoryListSz
     ),
     update=extend_schema(
         request=UpsertCategorySerializer,
-        responses=CategorySz
+        responses=CategoryListSz
     ),
 )
 class CategoryVS(ModelViewSet):
@@ -23,4 +23,14 @@ class CategoryVS(ModelViewSet):
     def get_serializer_class(self):
         if self.action in ["create", "update", "partial_update"]:
             return UpsertCategorySerializer
-        return CategorySz
+        return CategoryListSz
+
+
+@extend_schema_view(
+    responses=ProductSz,
+    request=ProductSz,
+)
+class ProductVS(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSz
+    parser_classes = (MultiPartParser, FormParser)
