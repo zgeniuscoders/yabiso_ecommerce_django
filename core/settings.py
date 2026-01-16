a220d93dfa463f8abcb38f67934da4fa911ece1a
zgeniusecoders <zgeniusecoders@gmail.com>
2026-01-16 08:00:57 +0100

    feat(settings): ajout de la pagination

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema'
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20,
 }
 
 SPECTACULAR_SETTINGS = {
